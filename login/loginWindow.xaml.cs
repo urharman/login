ea1c5bbd6120afecfca63a326b801dd00799fe00
Harmanpreet Singh <HS036@atcltd.atc.org.nz>
2019-02-06 15:08:04 -0800

    Form validation

--- login/loginWindow.xaml.cs
+++ login/loginWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,6 +24,39 @@ namespace login
         public loginWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-U551NOV\SQLEXPRESS;Initial Catalog=loginDB;Integrated Security=True");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblUser WHERE Username = @Username AND Password =@Password";
+                SqlCommand SqlCmd = new SqlCommand(query, sqlCon);
+                SqlCmd.CommandType = CommandType.Text;
+                SqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                SqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(SqlCmd.ExecuteScalar());
+                if (count ==1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                { MessageBox.Show("Username or Password is Incorrect");
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
     }
 }
